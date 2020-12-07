e8d94a1dfa523018ffd7e7d9eeeaa6aa78043409
Julie Choi <darthjuls@gmail.com>
2020-12-07 00:00:18 -0800

    fixed js and added popups for search and cart

--- popupforms.js
+++ popupforms.js
@@ -6,3 +6,17 @@ function openForm() {
 function closeForm() {
   document.getElementById("myForm").style.display = "none";
 }
+function openCart() {
+  document.getElementById("myCart").style.display = "block";
+}
+
+function closeCart() {
+  document.getElementById("myCart").style.display = "none";
+}
+function openSearch() {
+  document.getElementById("mySearch").style.display = "block";
+}
+
+function closeSearch() {
+  document.getElementById("mySearch").style.display = "none";
+}
